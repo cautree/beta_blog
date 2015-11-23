8878a2ca494a688c15fc216594683cd9a529486e
Yanyan Liu <yanyanliujs@gmail.com>
2015-11-23 04:50:26 +0000

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,5 @@ Rails.application.configure do
 end
 
 class Application < Rails::Application
-  config.web_console.whitelisted_ips = '18.111.79.209'
+  config.web_console.whitelisted_ips = '24.218.128.197'
 end
