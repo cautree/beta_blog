d1f91790eaa035c7f9a0349c3e78df0bfddcda38
Yanyan Liu <yanyanliujs@gmail.com>
2015-11-25 04:10:17 +0000

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'pages#home'
+  get 'about', to: 'pages#about'
   #get 'about', to: 'pages#about'
+  
   
   
   resources :articles
