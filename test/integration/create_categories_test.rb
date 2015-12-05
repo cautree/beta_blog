04845d2c6b3c8c99c4159c1565d8f7e3e67f98cd
Yanyan Liu <yanyanliujs@gmail.com>
2015-12-05 18:14:18 +0000

    add navibar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,11 +1,19 @@
 require 'test_helper'
+  
+
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user= User.create(username: "John", email: "john@example.com", password:"password", admin: true)
+    
+  end
   test "get new category form and create category" do
-    get new_category_path
-    assert_template 'categories/new'
-    assert_difference 'Category.count', 1 do
-      post_via_redirect categories_path, category: {name: "sports"}
+     sign_in_as(@user, "password")
+     get new_category_path
+     assert_template 'categories/new'
+     assert_difference 'Category.count', 1 do
+     post_via_redirect categories_path, category: {name: "sports"}
     end
     assert_template 'categories/index'
     assert_match "sports", response.body
@@ -13,9 +21,10 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     
     test "invalid category submission results in failure" do
+      sign_in_as(@user, "password")
       get new_category_path
-    assert_template 'categories/new'
-    assert_no_difference 'Category.count' do
+      assert_template 'categories/new'
+      assert_no_difference 'Category.count' do
       post_via_redirect categories_path, category: {name: " "}
     end
     assert_template 'categories/new'
